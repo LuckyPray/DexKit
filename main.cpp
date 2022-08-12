4511b92e9bcceee399599abadcaccfdc1ca20eed
teble <xie091030@qq.com>
2022-08-12 18:46:13 +0800

    add result example

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ int main() {
     auto now_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
 
     // 返回混淆map中包含所有字符串的类, 高级搜索可以使用 '^'与'$'限制字符串匹配，与正则表达式语义一致
+    // result ex.
+    // {"Lcom/tencent/mobileqq/troop/clockin/handler/TroopClockInHandler;" -> {"Lxadt;"}}
     auto res = dexKit.LocationClasses(obfuscate, true);
 //    dexKit.SetThreadNum(1);
     for (auto &[key, value]: res) {
@@ -35,6 +37,8 @@ int main() {
     // 返回所有通过invoke-kind指令调用指定的方法的方法描述符
     // 指定了方法签名后会自动填充后面的参数
     // 如果不指定方法签名则可按参数模糊匹配，使用空串则代表模糊匹配
+    // result ex.
+    // {"Lcom/qzone/album/ui/widget/AlbumDialog;->n(I)V"}
     auto res1 = dexKit.FindMethodInvoked("Landroid/widget/TextView;->setCompoundDrawablesWithIntrinsicBounds(IIII)V",
                                          "", "", "", v, p, false);
     std::cout << "FindMethodInvoked -> \n";
@@ -43,6 +47,8 @@ int main() {
     }
 
     // 返回所有指定类的子类，自动判断该类是否是接口或抽象类
+    // result ex.
+    // {"Landroidx/core/app/ComponentActivity;"}
     auto res2 = dexKit.FindSubClasses("Landroid/app/Activity;");
     std::cout << "FindSubClasses -> \n";
     for (auto &value: res2) {
@@ -51,6 +57,8 @@ int main() {
 
     std::vector<uint8_t> op_seq{0x70, 0x22, 0x70, 0x5b, 0x22, 0x70, 0x5b, 0x0e};
     // 返回所有符合字节码op序列前缀的方法描述符
+    // result ex.
+    // {"Lcom/bumptech/glide/d/c;-><init>()V"}
     auto res3 = dexKit.FindMethodOpPrefixSeq(op_seq, "Lcom/bumptech/glide/d/c;", "<init>", "void", {}, p, false);
     std::cout << "FindMethodOpPrefixSeq -> \n";
     for (auto &value: res3) {
@@ -59,6 +67,8 @@ int main() {
 
     // 返回所有使用了匹配字符串的方法描述符
     // 高级搜索可以使用 '^'与'$'限制字符串匹配，与正则表达式语义一致
+    // result ex.
+    // {"Lcom/tencent/aekit/openrender/internal/Frame$Type;-><clinit>()V"}
     auto res4 = dexKit.FindMethodUsedString("^NEW$", {}, {}, {}, {}, p, true, true);
     std::cout << "FindMethodUsedString -> \n";
     for (auto &value: res4) {
