592a1f3f838bbdc783dd889d9fd747f29f6ac0ca
teble <me@teble.me>
2023-01-15 08:43:55 +0800

    feat: add multi-platform run support
    Signed-off-by: teble <me@teble.me>

---
+++ Android/main/src/main/java/Main.kt
@@ -0,0 +1,42 @@
+import io.luckypray.dexkit.DexKitBridge
+import java.io.File
+import java.util.Locale
+
+val isWindows
+    get() = System.getProperty("os.name").lowercase(Locale.getDefault()).contains("windows")
+
+@Suppress("UnsafeDynamicallyLoadedCode")
+fun loadLibrary(name: String) {
+    val libName = if (isWindows) "lib$name" else name
+    try {
+        System.loadLibrary(libName)
+    } catch (e: UnsatisfiedLinkError) {
+        val libraryPath = File("main/build/library")
+        libraryPath.listFiles()?.forEach {
+            if (it.name.startsWith(libName)) {
+                System.load(it.absolutePath)
+            }
+        }
+    }
+}
+
+fun main() {
+    loadLibrary("dexkit")
+    println("current work dir: ${File("").absolutePath}")
+    val file = File("apk/QQ-play-8.2.11.apk")
+    if (!file.exists()) {
+        println("apk not found")
+        return
+    }
+    find(file.absolutePath)
+}
+
+fun find(path: String) {
+    DexKitBridge.create(path)?.use { kit ->
+        kit.findMethodUsingString {
+            usingString = "imei"
+        }.forEach {
+            println(it.descriptor)
+        }
+    }
+}
