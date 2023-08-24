fc649d97ce7000236cca84aba61529be2bb77d5a
teble <me@teble.me>
2023-08-25 02:32:42 +0800

    add some test

--- dexkit/src/test/java/io/luckypray/dexkit/UnitTest.kt
+++ dexkit/src/test/java/io/luckypray/dexkit/UnitTest.kt
@@ -73,4 +73,97 @@ class UnitTest {
         assert(mainActivity.getInterfaces().size == 1)
     }
 
+    @Test
+    fun testClassFieldsSearch() {
+        val res = bridge.findClass {
+            matcher {
+                fields {
+                    addForType("java.lang.String")
+                    addForType("android.widget.TextView")
+                    addForType("android.os.Handler")
+                    countRange(3)
+                }
+            }
+        }
+        println(res)
+        assert(res.size == 1)
+        val playActivity = res.first()
+        val fields = playActivity.getFields()
+        assert(fields.size == 3)
+        assert(playActivity.className == "org.luckypray.dexkit.demo.PlayActivity")
+    }
+
+    @Test
+    fun testFindClassUsingString() {
+        val res = bridge.findClass {
+            matcher {
+                useStrings("PlayActivity")
+            }
+        }
+        println(res)
+        assert(res.size == 1)
+        assert(res.first().className == "org.luckypray.dexkit.demo.PlayActivity")
+    }
+
+    @Test
+    fun testFindClassUsingStringArray() {
+        val res = bridge.findClass {
+            matcher {
+                useStrings("PlayActivity", "onClick")
+            }
+        }
+        println(res)
+        assert(res.size == 1)
+        assert(res.first().className == "org.luckypray.dexkit.demo.PlayActivity")
+    }
+
+    @Test
+    fun testFindClassSuper() {
+        val res = bridge.findClass {
+            matcher {
+                superClass("androidx.appcompat.app.AppCompatActivity")
+            }
+        }
+        println(res)
+        assert(res.size == 2)
+        res.map { it.className }.forEach {
+            assert(it.startsWith("org.luckypray.dexkit.demo"))
+            assert(it.endsWith("Activity"))
+        }
+    }
+
+    @Test
+    fun testFindClassImpl() {
+        val res = bridge.findClass {
+            searchPackage("org.luckypray.dexkit.demo")
+            matcher {
+                superClass("AppCompatActivity", StringMatchType.EndWith)
+                interfaces {
+                    add("android.view.View\$OnClickListener")
+                    countRange(1)
+                }
+            }
+        }
+        println(res)
+        assert(res.size == 1)
+        assert(res.first().className == "org.luckypray.dexkit.demo.MainActivity")
+    }
+
+    @Test
+    fun testFindClassImplIgnoreCase() {
+        val res = bridge.findClass {
+            searchPackage("org.luckypray.dexkit.demo")
+            matcher {
+                superClass("appcompatactivity", StringMatchType.EndWith, true)
+                interfaces {
+                    add("android.view.View\$OnClicklistener", StringMatchType.Equal, true)
+                    countRange(1)
+                }
+            }
+        }
+        println(res)
+        assert(res.size == 1)
+        assert(res.first().className == "org.luckypray.dexkit.demo.MainActivity")
+    }
+
 }
